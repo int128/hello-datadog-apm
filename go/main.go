5a9a8e236141c06bf099a7ea7ff143faeeb70b97
Hidetake Iwata <int128@gmail.com>
2023-03-26 21:57:08 +0900

    Wait for flush

--- go/main.go
+++ go/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-func do() {
-	span := tracer.StartSpan("run")
-	defer span.Finish()
-	log.Printf("Hello world")
+func do() error {
+	resp, err := http.Get("https://httpbin.org/get")
+	if err != nil {
+		return fmt.Errorf("failed to get: %w", err)
+	}
+	defer resp.Body.Close()
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read: %w", err)
+	}
+	log.Printf("got: %s", b)
+	return nil
 }
 
 func run() int {
@@ -21,8 +32,13 @@ func run() int {
 	)
 	defer tracer.Stop()
 
-	for i := 0; i < 50; i++ {
-		do()
+	var err error
+	span := tracer.StartSpan("run")
+	defer span.Finish(tracer.WithError(err))
+	err = do()
+	if err != nil {
+		log.Printf("error: %s", err)
+		return 1
 	}
 	return 0
 }
