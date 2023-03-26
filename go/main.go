6362ce2e35f01d0ecd26d5ddf387cc2f4450580d
Hidetake Iwata <int128@gmail.com>
2023-03-26 16:22:01 +0900

    Initial commit

---
+++ go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+)
+
+func run() int {
+	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
+	tracer.Start(tracer.WithService("hello-datadog-apm"))
+	defer tracer.Stop()
+
+	log.Printf("Hello world")
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
